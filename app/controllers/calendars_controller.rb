2f0716ed43d060374636e3e9255ccff93f690696
ushiyushi <ymurae46@gmail.com>
2025-06-01 19:08:32 +0900

    divとハッシュロケットの修正

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -41,7 +41,7 @@ class CalendarsController < ApplicationController
       end
       wday = wdays[wday_num]
 
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans, :wday => wday }
+      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wday: wday }
       @week_days.push(days)
     end
 
